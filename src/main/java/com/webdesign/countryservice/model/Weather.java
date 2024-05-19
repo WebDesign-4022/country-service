43146f4ffd54ba88dd656f08eb4f585731a6a4d2
dshmmd <mohammadzarea.bidaki@gmail.com>
2024-05-19 15:29:43 +0330

    remove redundant getter/setters

--- src/main/java/com/webdesign/countryservice/model/Weather.java
+++ src/main/java/com/webdesign/countryservice/model/Weather.java
@@ -1,58 +1,19 @@
 package com.webdesign.countryservice.model;
 
 public class Weather {
-    private String countryName;
-    private String capital;
-    private Double windSpeed;
-    private Integer windDegrees;
-    private Integer temp;
-    private Integer humidity;
+    private final String countryName;
+    private final String capital;
+    private float windSpeed;
+    private int windDegrees;
+    private int temp;
+    private int humidity;
 
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(String countryName) {
+    public Weather(String countryName, String capital, float windSpeed, int windDegrees, int temp, int humidity) {
         this.countryName = countryName;
-    }
-
-    public String getCapital() {
-        return capital;
-    }
-
-    public void setCapital(String capital) {
         this.capital = capital;
-    }
-
-    public Double getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(Double windSpeed) {
         this.windSpeed = windSpeed;
-    }
-
-    public Integer getWindDegrees() {
-        return windDegrees;
-    }
-
-    public void setWindDegrees(Integer windDegrees) {
         this.windDegrees = windDegrees;
-    }
-
-    public Integer getTemp() {
-        return temp;
-    }
-
-    public void setTemp(Integer temp) {
         this.temp = temp;
-    }
-
-    public Integer getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(Integer humidity) {
         this.humidity = humidity;
     }
 }
